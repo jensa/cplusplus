4f9815a590245c4f78dade2868ec272daecf1943
jensarv <jensarv@gmail.com>
2013-09-09 19:04:18 +0200

    first assignment

--- lab1/hello.cpp
+++ lab1/hello.cpp
@@ -1,1 +1,31 @@
-fuck aina
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+	if (argc < 2){
+		cout << "Hello World";
+	} else {
+		string name;
+		int num = atoi (argv[1]);
+		if (num == 0){
+			name = string (argv[1]);
+			string zero ("0");
+			if (zero.compare(name) == 0){
+				name = "";
+			}
+			cout << "Hello " << name;
+		} else{
+			name = string (argv[2]);
+			cout << "Hello ";
+			for (int i = 0; i < num-1; ++i)
+			{
+				cout << name << " ";
+			}
+			cout << name;
+		}
+	}
+	cout << "!\n";
+}
