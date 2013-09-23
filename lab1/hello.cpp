cb013559ba798329ff8ca0a13587f51828c9f5f1
jens <jens@ubuntu.(none)>
2013-09-23 14:43:33 +0200

    allt bra, inget dåligt

--- lab1/hello.cpp
+++ lab1/hello.cpp
@@ -8,7 +8,7 @@ int main (int argc, char *argv[]){
 	if (argc < 2){
 		cout << "Hello World";
 	} else {
-		string name;
+		string name ("");
 		int num = atoi (argv[1]);
 		if (num == 0){
 			name = string (argv[1]);
@@ -18,7 +18,10 @@ int main (int argc, char *argv[]){
 			}
 			cout << "Hello " << name;
 		} else{
-			name = string (argv[2]);
+			if (argv[2] == NULL)
+				name = string("World");
+			else
+				name = string (argv[2]);
 			cout << "Hello ";
 			for (int i = 0; i < num-1; ++i)
 			{
