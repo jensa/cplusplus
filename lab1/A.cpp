01feda89d18b7e34e89dd84bcf089048bf1002b0
Ludvig Jonsson <jonsson.ludde@gmail.com>
2013-09-25 12:52:42 +0200

    Uppgift 1.3, valgrind vad händz

--- lab1/A.cpp
+++ lab1/A.cpp
@@ -1,35 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
+using namespace std;
+
 class A {
+    
 public:
     A() 
-        {std::cout << "The default contructor" << std::endl; }
+        {cout << "The default contructor" << "\n"; }
     A(const A & ref) 
-        {std::cout << "The copy contructor" << std::endl; }
+        {cout << "The copy contructor" << "\n"; }
     ~A() 
-        {std::cout << "The destructor" << std::endl; }
+        {cout << "The destructor" << "\n"; }
     A(char * s) 
-        {std::cout << "Some other constructor " << s << std::endl;}
+        {cout << "Some other constructor " << s << "\n";}
     A & operator=(const A & s) 
-        {std::cout << "The assignment operator" << std::endl; 
+        {cout << "The assignment operator" << "\n"; 
          return *this;}
 };
 
-void no_ref(A a) {}
-void with_ref(const A & a) {}
+void no_ref(A a) {cout << "a in no_ref: " << &a << "\n";}
+void with_ref(const A & a) {cout << "a in with_ref: " << &a << "\n";}
 
 int main()
 {
+    cout << 1 << "\n";
     A a("my name is a");
+    cout << "a: " << &a << "\n";
+
+    cout << 2 << "\n";
     A b = a;         // vad är skillnaden
+    cout << "b: " << &b << "\n";
+
+    cout << 3 << "\n";
     A c(a);          // mellan dessa
+    cout << "c: " << &c << "\n";
+
+    cout << 4 << "\n";
     A d;             // tre tekniker?
+    cout << "d: " << &d << "\n";
+
+    cout << 5 << "\n";
     d = a;
+    cout << "d: " << &d << "\n";
+    cout << "a: " << &a << "\n";
 
-    no_ref(a);       // Bildas temporära objekt?
-    with_ref(a);     // Bildas temporära objekt?
+    cout << 6 << "\n";
+    cout << "a: " << &a << "\n";
+    no_ref(a);       // Bildas temporära objekt? Ja, och sedan kallas destruktorn.
+    cout << 7 << "\n";
+    cout << "a: " << &a << "\n";
+    with_ref(a);     // Bildas temporära objekt? Nope.
 
+    cout << 8 << "\n";
     A *aa = new A[5];
-    delete aa;       // Vad kommer att hända?
+
+    cout << 9 << "\n";
+    delete[] aa;       // Vad kommer att hända?
     return 0;
 }
---
+++ lab1/Data.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+struct Data {
+    int x, y, z;
+};  
+
+Data ** foo(Data ** v, int x) {
+    for (int i = 0; i < x; i++)
+        // if (v[i] != 0)
+        {
+			v[i] = new Data;
+        	//delete v[i];
+        }
+    return v;
+}
+
+int main () {
+    const int size = 5;
+    Data ** v = new Data * [size];
+    Data ** p = foo(v, size);
+    delete[] p;
+}
