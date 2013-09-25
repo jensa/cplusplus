9c6158104794090b467234b08ae34b12d2cdc2a8
Jens Arvidsson <jensarv@gmail.com>
2013-09-25 13:53:38 +0200

    balle

--- lab1/Vector.cpp
+++ lab1/Vector.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
+#include <stdexcept>
+
 #define MUNK = NULL
+
+
 
 class Vector
 {
 public:
-	unsigned int array[];
+	unsigned int *array;
 	size_t length;
 
 	Vector (size_t size){
 		length = size;
-		array = new unsigned[length];
+		array = new unsigned int[length];
 		for (int i = 0; i < length; ++i)
 		{
 			array[i] = 0;
 		}
 	}
 
-    unsigned length (){
-    	return length;
-    }
-
-    unsigned get (int index){
-    	if (index > length || index < 0)
-    		throw std::out_of_range;
+    unsigned int get (int index){
+    	if (index > length-1 || index < 0)
+    		throw std::out_of_range("Out of range!");
     	return array[index];
     }
 
     Vector & operator=(const Vector & s) {
-		if (s.length () != length){
+		if (s.length != length){
 			delete[] array;
-			array = new unsigned[s.length ()];
+			array = new unsigned[s.length];
 		}
-		for (int i = 0; i < s.length (); ++i)
+		for (int i = 0; i < s.length; ++i)
 		{
 			array[i] = s.get(i);
 		}
-        return this;
-    }
-
-    size_t length (){
-    	return length;
-    }
-
-    unsigned get (int index){
-    	if (index > length-1 || index < 0)
-    		throw std::out_of_range;
-    	return array[index];
+        return *this;
     }
 };
