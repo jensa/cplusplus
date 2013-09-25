43d33b9c15e8f523c159e09f199e0686a7dc9a23
Jens Arvidsson <jensarv@gmail.com>
2013-09-25 14:22:39 +0200

    yo

---
+++ lab1/vector.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdexcept>
+
+#define MUNK = NULL
+
+
+
+class Vector
+{
+public:
+	unsigned int *array;
+	int length;
+
+	Vector (int size){
+		length = size;
+		array = new unsigned int[length];
+		for (int i = 0; i < length; ++i)
+		{
+			array[i] = 0;
+		}
+	}
+
+	//Copy contructor
+	Vector (const Vector & vec){
+		fitArray (vec);
+		for (int i = 0; i < vec.length; ++i)
+		{
+			array[i] = vec.get(i);
+		}
+	}
+
+    unsigned int get (int index) const{
+    	if (index > length-1 || index < 0)
+    		throw std::out_of_range("Out of range!");
+    	return array[index];
+    }
+
+    Vector & operator=(const Vector & vec) {
+		fitArray (vec);
+		for (int i = 0; i < vec.length; ++i)
+		{
+			array[i] = vec.get(i);
+		}
+        return *this;
+    }
+
+    Vector &operator[](const int & index){
+    	return get (index);
+    }
+
+private:
+	void fitArray (const Vector & vec){
+		if (vec.length != length){
+			delete[] array;
+			array = new unsigned[vec.length];
+		}
+	}
+};
