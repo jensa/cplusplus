e1931afe91f0e5294017e3b7457164f9e3007040
Ludvig Jonsson <jonsson.ludde@gmail.com>
2013-09-28 12:51:38 +0200

    1.5 - Templates funkar. Yoco.

---
+++ lab1/1.5/test_cxx.cpp
@@ -0,0 +1,165 @@
+#include <cxxtest/TestSuite.h>
+
+#include "vector.h"
+
+class MyTestSuite : public CxxTest::TestSuite 
+{
+public:
+
+    void test_initialized_as_zero( void )
+    {
+        Vector<int> a(1);
+        int result = a[0];
+        TS_ASSERT_EQUALS(result, 0);
+    }
+
+    void test_vector_with_length_zero( void )
+    {
+        Vector<int> a(0);
+        int length = a.size();
+        TS_ASSERT_EQUALS(length, 0);   
+    }
+
+    void test_set ( void )
+    {
+        Vector<int> a(1);
+        TS_ASSERT_EQUALS(a[0], 0);
+        a[0] = 5;
+        TS_ASSERT_EQUALS(a[0], 5);
+    }
+
+    void test_negative_index ( void )
+    {
+        Vector<int> a(5);
+        TS_ASSERT_THROWS(a[-1], std::out_of_range)
+    }
+
+    void test_out_of_range ( void )
+    {
+        Vector<int> a(5);
+        TS_ASSERT_THROWS(a[5], std::out_of_range)
+    }
+
+    void test_copy_equal_lengths( void )
+    {
+        int length = 10000;
+        Vector<int> a(length);
+        Vector<int> b(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            a[i] = i+1;
+        }
+
+        b = a;
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            TS_ASSERT_EQUALS(a[i],b[i]);
+        }
+    }
+
+    void test_copy_a_to_a ( void )
+    {
+        Vector<int> a(3);
+        Vector<int>* ptr = &a;
+        
+        a = a;
+
+        TS_ASSERT_EQUALS(ptr, &a);
+    }
+
+    void test_copy_a_bigger ( void )
+    {
+        Vector<int> a(100);
+        Vector<int> b(50);
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            a[i] = i+1;
+        }
+
+        b = a;
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            TS_ASSERT_EQUALS(a[i],b[i]);
+        }
+    }
+
+    void test_copy_b_bigger ( void )
+    {
+        Vector<int> a(50);
+        Vector<int> b(100);
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            a[i] = i+1;
+        }
+
+        b = a;
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            TS_ASSERT_EQUALS(a[i],b[i]);
+        }
+    }
+
+    void test_copy_constructor (void)
+    {
+        Vector<int> a(50);
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            a[i] = i+1;
+        }
+
+        Vector<int> b = a;
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            TS_ASSERT_EQUALS(a[i],b[i]);
+        }
+
+        Vector<int> c(a);
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            TS_ASSERT_EQUALS(a[i],c[i]);
+        }
+    }
+
+    void test_move_constructor (void)
+    {
+        Vector<int> a(5);
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+
+        Vector<int> b = std::move(a);
+
+        TS_ASSERT_EQUALS(a.size(), 0);
+        TS_ASSERT_EQUALS(b.size(), 5);
+        TS_ASSERT_EQUALS(b[0], 1);
+        TS_ASSERT_EQUALS(b[1], 2);
+    }
+
+    void test_move_assignment_operator (void)
+    {
+        Vector<int> a(5); 
+        a = {1,1,1,1,1};
+
+        Vector<int> b(5); 
+        b = {2,2,2,2,2};
+
+        a = std::move(b);
+
+        TS_ASSERT_EQUALS(b.size(),0);
+        TS_ASSERT_EQUALS(a.size(),5);
+
+        for (int i = 0; i < a.size(); i++)
+            TS_ASSERT_EQUALS(a[i],2);
+    }
+};
---
+++ lab1/1.5/vector.h
@@ -0,0 +1,121 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <cstdlib>
+#include <stdexcept>
+#include <iostream>
+#include <initializer_list>
+
+template <class T>
+class Vector
+{
+	public:
+		size_t length;
+		T *array;
+
+		// Constructors
+
+		Vector() {
+			length = (size_t) 0;
+			array = new T[0];
+		}
+
+		explicit Vector (size_t size) {
+			length = size;
+			array = new T[length]();
+		}
+
+		Vector (const Vector & vec) {
+			length = vec.size();
+		    array = new T[length];
+		    for (int i = 0; i < (int) length; ++i) {
+		        array[i] = vec[i];
+		    }
+		}
+
+		Vector (Vector&& other) {
+			array = other.array;
+			length = other.length;
+
+			other.array = NULL;
+			other.length = 0;
+		}
+
+		// Destructor
+
+		~Vector () {
+			delete[] array;
+		}
+
+		size_t size() const {
+			return length;
+		}
+
+		void print() {
+			std::cout << "[";
+			for (int i = 0; i < (int) length; i++){
+				std::cout << array[i];
+				if (i != (int) length-1)
+					std::cout << ",";
+			}
+			std::cout << "]" << "\n";
+		}
+
+		// [] operators
+		
+		T & operator[](unsigned int index) {
+			if (index >= length || index < 0)
+			throw std::out_of_range("Out of range!");
+			return array[index];
+		}
+
+		const T & operator[](unsigned int index) const {
+			if (index >= length || index < 0)
+			throw std::out_of_range("Out of range!");
+			return array[index];
+		}
+
+		// = operators
+
+		Vector & operator=(const Vector & vec) {
+			if (this != &vec) {
+				delete[] array;
+				array = new T[vec.size()];
+				length = vec.size();
+				for (int i = 0; i < (int) vec.size(); ++i) {
+					array[i] = vec[i];
+				}
+			}
+			return *this;	
+		}
+
+		Vector & operator=(Vector && other) {
+				if (this != &other)
+				{
+					delete[] array;
+					array = other.array;
+					length = other.length;
+
+					other.array = NULL;
+					other.length = 0;
+				}
+				return *this;
+		}
+
+		Vector & operator=(const std::initializer_list<T>& il) {
+			delete[] array;
+			array = new T[il.size()];
+			length = il.size();
+
+			typename std::initializer_list<T>::iterator it;
+			int i = 0;
+
+			for (it = il.begin(); it != il.end(); it++, i++) {
+				array[i] = *it;
+			}
+
+			return *this;
+		}
+};
+
+#endif
