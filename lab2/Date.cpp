a38159e0b64a7ab28c678a24611d48d79a028f08
Jens Arvidsson <jensarv@gmail.com>
2013-10-16 16:31:40 +0200

    Ballz

--- lab2/Date.cpp
+++ lab2/Date.cpp
@@ -5,22 +5,15 @@ namespace lab2 {
 
 class Date {
 
-	int year_v;
-	int month_v;
-	int day_v;
-	int days_per_week_v;
-	int months_per_year_v;
-
 	public:
-	 int year (){ return year_v;}
-	 int month () {return month_v;}
-	 int day () { return day_v;}
-	 int days_per_week () { return days_per_week_v;}
-	 int months_per_year () {return months_per_year_v;}
 	 virtual int week_day () = 0;
 	 virtual int days_this_month () = 0;
 	 virtual std::string week_day_name ();
 	 virtual std::string month_name ();
+
+	 Date & Date::operator++(const Date & date) {
+
+	 	 }
 };
 }
 
---
+++ lab2/Date.h
@@ -0,0 +1,66 @@
+#include <iostream>
+
+namespace lab2 {
+
+
+class Date {
+
+	int year_v;
+	int month_v;
+	int day_v;
+	int days_per_week_v;
+	int months_per_year_v;
+
+	public:
+	 int year (){ return year_v;}
+	 int month () {return month_v;}
+	 int day () { return day_v;}
+	 int days_per_week () { return days_per_week_v;}
+	 int months_per_year () {return months_per_year_v;}
+	 //returns the current year after the operation
+	 int add_year (int n){
+	 	year_v += n;
+	 	return year_v;
+	 }
+	 virtual int week_day () = 0;
+	 virtual int days_this_month () = 0;
+	 virtual std::string week_day_name ();
+	 virtual std::string month_name ();
+	 
+	 //returns the current month after the operation
+	 virtual int add_month (int n);
+
+	 lab2::Date & operator++() {
+	 	increment_day (1);
+	 	return *this;
+ 	 };
+
+ 	 lab2::Date & operator++(int wat) {
+	 	increment_day (1);
+	 	return *this;
+ 	 };
+
+ 	 lab2::Date & operator--(){
+ 	 	decrement_day(1);
+ 	 	return *this;
+ 	 }
+ 	 lab2::Date & operator +=(int num){
+ 	 	increment_day(num);
+ 	 	return *this;
+ 	 }
+
+ 	 lab2::Date & operator -=(int num){
+ 	 	decrement_day(num);
+ 	 	return *this;
+ 	 }
+
+	protected:
+	 virtual void increment_day (int num);
+	 virtual void decrement_day (int num);
+
+	 
+};
+}
+
+
+
