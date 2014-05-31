8547b9fd4dc94c7b04790d1fc3fba3c86ae8b1f7
Jens Arvidsson <jensarv@gmail.com>
2014-05-31 14:16:41 +0200

    direciton and environment

---
+++ lab3/direction.h
@@ -0,0 +1,8 @@
+namespace lab3{
+	enum direction {
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST
+	} dir_object_names;
+}
---
+++ lab3/environment.cpp
@@ -0,0 +1,22 @@
+
+namespace lab3 {
+	std::vector<Item> items;
+	std::vector<std::string> neighbors;
+
+	Environment::Environment(){
+		items = new std::vector<Item>();
+	}
+
+	std::string directions(){
+
+	}
+
+neighbor(direction)
+description()
+enter(character)
+leave(character)
+pick_up(object)
+drop(object)
+
+}
+
