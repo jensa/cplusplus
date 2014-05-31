837a18b73b30a5555efb26910460f7576eea8b42
Jens Arvidsson <jensarv@gmail.com>
2014-05-31 16:28:32 +0200

    enviroments

--- lab3/environment.cpp
+++ lab3/environment.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include <vector>
+#include "environment.h"
 
 namespace lab3 {
 	//std::vector<Item> items;
+	std::map <std::string, Environment> neighbor_map;
 	std::vector<std::string> neighbors;
+	std::string description;
 
-	Environment::Environment(){
+	Environment::Environment(std::string desc){
+		description = desc;
 		//items = new std::vector<Item>();
 	}
 
-	std::string Encironment::directions(){
-		std::string toReturn = "possible directions are: "
+	std::string Environment::directions(){
+		std::string toReturn = "possible directions are: ";
 		for (std::vector<std::string>::iterator i = neighbors.begin(); i != neighbors.end(); ++i)
 		{
-			toReturn += i;
+			toReturn += *i;
 		}
 		return toReturn;
+	}
+
+	Environment Environment::neighbor(std::string direction){
+		return neighbor_map[direction];
+	}
+	
+	std::string Environment::description() const{
+		return description;
+	}
+	
+	void Enviroment::enter(Character c){
+
+	}
+
+	void Environment::leave(Character c){
+
+	}
+
+	void Environment::pick_up(Object o){
+
+	}
+
+	void Environment::drop(Object o){
+
 	}
 
 }
