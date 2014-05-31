3b882fafd27ebd1006b0e0e1af5adf4388f5da4f
Ludvig Jonsson <jonsson.ludde@gmail.com>
2014-05-31 16:43:00 +0200

    Added Character classes

---
+++ lab3/npc.h
@@ -0,0 +1,14 @@
+#ifndef NPCDEF
+#define NPCDEF
+#include "character.h"
+#include <iostream>
+
+namespace lab3 {
+	class NPC : public Character {
+		public:
+			virtual void action() = 0;
+			virtual void fight(Character&) = 0;
+			virtual void talk_to(Character&) = 0;
+	};
+}
+#endif
